8eed998acff860e3e9a14946628138a3bd65a822
Eric Larson <larson.eric.d@gmail.com>
2022-02-14 14:46:36 -0500

    ENH: Path

--- auto.sh
+++ auto.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ef
 
+THIS_DIR = $(dirname "$0")
 case "$OSTYPE" in
-	darwin*)  echo "macOS ($OSTYPE)" && macos/setup_sound.sh ;; 
-	linux*)   echo "Linux ($OSTYPE)" && linux/setup_sound.sh ;;
-	*)        echo "Windows ($OSTYPE)" && powershell windows/setup_sound.ps1 ;;
+	darwin*)  echo "macOS ($OSTYPE)" && $THIS_DIR/macos/setup_sound.sh ;;
+	linux*)   echo "Linux ($OSTYPE)" && $THIS_DIR/linux/setup_sound.sh ;;
+	*)        echo "Windows ($OSTYPE)" && powershell $THIS_DIR/windows/setup_sound.ps1 ;;
 esac
