7cf464d132c5a6be5ad80d7089ff390c572c189e
Eric Larson <larson.eric.d@gmail.com>
2022-02-14 14:48:17 -0500

    FIX: Syntax

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ef
 
-THIS_DIR = $(dirname "$0")
+THIS_DIR=$(dirname "$0")
 case "$OSTYPE" in
 	darwin*)  echo "macOS ($OSTYPE)" && $THIS_DIR/macos/setup_sound.sh ;;
 	linux*)   echo "Linux ($OSTYPE)" && $THIS_DIR/linux/setup_sound.sh ;;
