fe2eca32b46d673f6afa14bb8b0aa2f499065047
Eric Larson <larson.eric.d@gmail.com>
2022-10-28 11:36:09 -0400

    FIX: Protocol

--- macos/setup_sound.sh
+++ macos/setup_sound.sh
@@ -2,6 +2,6 @@
 
 curl -L https://github.com/downloads/AVbin/AVbin/AVbin10.pkg -o ~/AVbin10.pkg
 sudo installer -pkg ~/AVbin10.pkg -target /
-git clone git://github.com/kyleneideck/BackgroundMusic --depth=50
+git clone https://github.com/kyleneideck/BackgroundMusic --depth=50
 sudo xcodebuild -project BackgroundMusic/BGMApp/BGMAppTests/NullAudio/AudioDriverExamples.xcodeproj -target NullAudio DSTROOT="/" install
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
