30759719191431ca3af36200d722162a5ce0cb98
Eric Larson <larson.eric.d@gmail.com>
2023-06-30 09:27:52 -0400

    MAINT: Remove avbin (#9)
    * MAINT: Remove avbin
    
    * FIX: Ver
    
    * ENH: Action

--- macos/setup_sound.sh
+++ macos/setup_sound.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-curl -L https://github.com/downloads/AVbin/AVbin/AVbin10.pkg -o ~/AVbin10.pkg
-sudo installer -pkg ~/AVbin10.pkg -target /
 git clone https://github.com/kyleneideck/BackgroundMusic --depth=50 -b 0.3.x
 sudo xcodebuild -project BackgroundMusic/BGMApp/BGMAppTests/NullAudio/AudioDriverExamples.xcodeproj -target NullAudio DSTROOT="/" install
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
