b9c4f3c000f0ae339c0280b0b101774d63181f60
Eric Larson <larson.eric.d@gmail.com>
2024-04-18 15:28:48 -0400

    MAINT: Get CIs green (#13)
    * FIX: v4
    
    * MAINT: Get green
    
    * FIX: More

--- linux/setup_sound.sh
+++ linux/setup_sound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 sudo apt update
-sudo apt install -y pulseaudio libportaudio2 dbus-x11
+sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 dbus-launch pulseaudio --start
