22e7c264b1a49170d2515947c84313308dd168a5
Eric Larson <larson.eric.d@gmail.com>
2024-04-18 15:31:32 -0400

    FIX: Simpler

--- macos/setup_sound.sh
+++ macos/setup_sound.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
-git clone https://github.com/kyleneideck/BackgroundMusic --depth=50 -b 0.3.x
-sudo xcodebuild -project BackgroundMusic/BGMApp/BGMAppTests/NullAudio/AudioDriverExamples.xcodeproj -target NullAudio DSTROOT="/" install
+brew install --cask background-music
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
