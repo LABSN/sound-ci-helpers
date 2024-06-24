330581c77d5f9c72daf833afc434a20cb8509d97
Eric Larson <larson.eric.d@gmail.com>
2024-06-24 12:54:52 -0400

    FIX: Check

--- linux/setup_sound.sh
+++ linux/setup_sound.sh
@@ -6,4 +6,4 @@ sudo apt update
 sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 systemctl --user restart pulseaudio.service
 systemctl --user restart pulseaudio.socket
-pulseaudio --check
+pactl list
