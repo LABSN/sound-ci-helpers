6a2b53ed67765e61e581a63b798a11e15759d990
Eric Larson <larson.eric.d@gmail.com>
2024-06-24 12:57:15 -0400

    ENH: Cleaner bash (#17)
    * ENH: Cleaner bash
    
    * FIX: More
    
    * FIX: Check

--- linux/setup_sound.sh
+++ linux/setup_sound.sh
@@ -4,4 +4,6 @@ set -xeo pipefail
 
 sudo apt update
 sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
-dbus-launch pulseaudio --start
+systemctl --user restart pulseaudio.service
+systemctl --user restart pulseaudio.socket
+pactl list
