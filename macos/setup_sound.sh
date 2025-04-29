a56da6ffd05ba99dfc2f278d1238f8f4ecafc625
Eric Larson <larson.eric.d@gmail.com>
2025-04-29 13:18:16 -0400

    FIX: Retry

--- macos/setup_sound.sh
+++ macos/setup_sound.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -xeo pipefail
+brew fetch --retry --cask background-music
 brew install --cask background-music
 sudo launchctl kickstart -kp system/com.apple.audio.coreaudiod || sudo killall coreaudiod
